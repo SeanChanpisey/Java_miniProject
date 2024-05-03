4428d73989ffd421b18a1681cea485b3a1d1392a
EI CHANUDOM <chanudomei122@gmail.com>
2024-05-03 15:29:00 +0700

    Test

--- MiniProject.java
+++ MiniProject.java
@@ -2,5 +2,8 @@ package Homework;
 
 public class MiniProject {
     public static void main(String[] args){
+        int number = 10;
+        int remainder = number % 2;
+        System.out.println(remainder);
     }
 }
