3f9c23acca12850f5eca8f237a2e8d9f18ca273b
SeanChanpisey <157389641+SeanChanpisey@users.noreply.github.com>
2024-05-15 11:10:58 +0700

    Delete MiniProject.java

--- MiniProject.java
+++
@@ -1,9 +0,0 @@
-package Homework;
-
-public class MiniProject {
-    public static void main(String[] args){
-        int number = 10;
-        int remainder = number % 2;
-        System.out.println(remainder);
-    }
-}
